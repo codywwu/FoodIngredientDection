db7b30cfa1223d1e7424ec68672015dfcc5a7c7b
codywwu <1905037781@qq.com>
2024-10-31 09:40:13 -0400

    Initial Commit

---
+++ FoodDectection/FoodDectectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDectectionApp.swift
+//  FoodDectection
+//
+//  Created by CODY WU on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodDectectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FoodDectectionTests/FoodDectectionTests.swift
@@ -0,0 +1,17 @@
+//
+//  FoodDectectionTests.swift
+//  FoodDectectionTests
+//
+//  Created by CODY WU on 10/31/24.
+//
+
+import Testing
+@testable import FoodDectection
+
+struct FoodDectectionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
